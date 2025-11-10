e558c64b7b268a9b7778853ae7c933f99e48d3d5
Santhosh <santhoshprogrammer07@gmail.com>
2025-11-10 21:48:53 +0530

    added API url in env file

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: API_URL
 });
